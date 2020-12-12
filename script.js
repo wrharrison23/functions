8c561886be71315649c2525f15bfd16bd5291d45
Wes <wrharrison23@gmail.com>
2020-12-12 11:57:09 -0500

    finished lightning 2

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hi')
+// Foundations - Functions
+
+// Lightning 1
+
+function logJSString() {
+    console.log("I love JavaScript!")
+}
+
+logJSString()
+
+// Lightning 2
+
+function vacation(name, destination) {
+    console.log(`${name} would love to visit ${destination}.`)
+}
+
+vacation("Wes", "Rome")
+vacation('Kayla', 'New York City')
+vacation('Drew', 'France')
